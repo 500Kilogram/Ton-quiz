f796afe42549d26f3163cf368cd8afece4392565
Ivan Cosinov <cosinovi@gmail.com>
2024-04-25 18:56:13 +0300

    1

---
+++ main.py
@@ -0,0 +1,16 @@
+from aiogram import Bot, types, Dispatcher, executor
+from aiogram.types.web_app_info import WebAppInfo
+
+
+bot = Bot(token='1945345757:AAHvxmD31AKVm3okGCzW-dW0ctAZe8DUIKg')
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def cmd_start(message: types.Message):
+    markup = types.ReplyKeyboardMarkup()
+    markup.add(types.KeyboardButton('Открыть веб', web_app=WebAppInfo(url='https://google.com')))
+    await message.answer('Привет', reply_markup=markup)
+
+
+executor.start_polling(dp)
